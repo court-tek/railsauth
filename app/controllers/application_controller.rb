10668814b9a7b6d9fe8fc5dea0cbb8bcc1a13d8e
Courtney Girley <courtneygirley@Courtneys-MacBook-Pro.local>
2023-10-23 16:02:27 -0400

    setup user auth models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    
+    private
+        def login(user)
+           Current.user = user
+           reset_session
+           session[:user_id] = user.id
+        end
+
+        def logout(user)
+            Current.user = nil
+            reset_session
+        end
 end
